65b0b959cff1543950ced11b9d29d68636eaac3e
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-11-15 12:19:19 +1000

    Made initial set up.

---
+++ selfstorage/selfstorage/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+
+def mainpage(request):
+    context = {
+        'key': 'value',
+    }
+    return render(request, 'index.html', context)
+
+
+def faqpage(request):
+    context = {
+        'key': 'value',
+    }
+    return render(request, 'faq.html', context)
+
+
+def boxespage(request):
+    context = {
+        'key': 'value',
+    }
+    return render(request, 'boxes.html', context)
+
+
+def myrentpage(request):
+    context = {
+        'key': 'value',
+    }
+    return render(request, 'my-rent.html', context)
+
+
+def myrentemptypage(request):
+    context = {
+        'key': 'value',
+    }
+    return render(request, 'my-rent-empty.html', context)
