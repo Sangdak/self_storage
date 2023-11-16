e5681d6ae5bcb27335bf2f7ed3d58b43b67e8b35
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-11-16 10:30:31 +1000

    Rewrite custom User model to Client model, add phonenumber field.

--- selfstorage/selfstorage/models.py
+++ selfstorage/selfstorage/models.py
@@ -1,6 +1,9 @@
-from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils import timezone
+
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class StoreHouse(models.Model):
@@ -31,13 +34,25 @@ class Box(models.Model):
         return self.title
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    name = models.TextField()
-    phone = models.CharField()
-    email = models.EmailField()
-    photo = models.ImageField()
+class Client(AbstractUser):
+    email = models.EmailField(
+        max_length=50,
+        unique=True,
+        verbose_name='E-mail',
+    )
+    phone_number = PhoneNumberField(
+        unique=True,
+        blank=True,
+        region='RU',
+        verbose_name='Номер телефона'
+    )
+    image = models.ImageField(
+        verbose_name='Аватар',
+    )
 
     def __str__(self):
-        return 'Profile for user {}'.format(self.user.username)
+        return f'{self.last_name} {self.first_name} - {self.phone_number}'
 
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
