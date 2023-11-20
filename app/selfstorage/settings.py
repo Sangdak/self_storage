e9ce8d4067e214b6151d63d9ca6a26a12bd13877
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-11-20 17:09:41 +1000

    Fixed environs import in settings.

--- app/selfstorage/settings.py
+++ app/selfstorage/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import environ
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 env = environ.Env()
-env.read_env(overwrite=True)
+env.read_env(os.path.join(BASE_DIR, '..', '.env'))
 
 
 # Quick-start development settings - unsuitable for production
