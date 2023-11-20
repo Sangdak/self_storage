0b54948bf6bb9f86fb52655e95836a79d0706ac1
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-11-20 19:25:05 +1000

    Fixed templates dir setup.

--- app/selfstorage/settings.py
+++ app/selfstorage/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'app/templates',
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
