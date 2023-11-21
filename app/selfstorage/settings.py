bd3de9d92d616dddb2121d019282dbe5d38586c5
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-11-21 17:58:28 +1000

    Removed pg_vars file from django settings.

--- app/selfstorage/settings.py
+++ app/selfstorage/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '..', '.env'))
-env.read_env(os.path.join(BASE_DIR, '..', '.env_db'))
 
 
 # Quick-start development settings - unsuitable for production
